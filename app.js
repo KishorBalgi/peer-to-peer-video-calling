b22fad83bf4100b2192751224de2af97b9f1360a
Kishor Balgi <kishorbalgi@gmail.com>
2024-05-11 10:14:00 +0530

    feat: output read

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { initSocketIO } from "./src/socketConfig.js";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 export const httpServer = createServer(app);
@@ -13,6 +15,18 @@ app.set("io", io);
 
 app.get("/", (req, res, next) => {
   res.send("<h1>Hello World!</h1>");
+});
+
+// Serve the output.txt file:
+app.get("/output", (req, res, next) => {
+  const filePath = path.join("public", "output.txt");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading file");
+      return;
+    }
+    res.send(data);
+  });
 });
 
 initSocketIO(io);
